fd5066803a4d0fb0752b757bcae526356a7696c3
KAPIL KUMAR BANSHEEWAL <88656401+kapilkumar12@users.noreply.github.com>
2023-01-25 23:02:27 +0530

    charts

--- src/component/dashboard/Dashboard.jsx
+++ src/component/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // import SemiCircleProgressBar from "react-progressbar-semicircle";
 
@@ -24,6 +24,8 @@ import FlagIcon from "@mui/icons-material/Flag";
 import NotificationAddIcon from "@mui/icons-material/NotificationAdd";
 import SettingsIcon from "@mui/icons-material/Settings";
 import MenuIcon from "@mui/icons-material/Menu";
+import Chart from "react-apexcharts";
+import ReactApexChart from "react-apexcharts";
 
 const Dashboard = () => {
   const toggle = () => {
@@ -34,6 +36,86 @@ const Dashboard = () => {
       x.style.display = "none";
     }
   };
+  const [state, setState] = useState({
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+      },
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91],
+      },
+    ],
+  });
+
+  const [spchart, setSpchart] = useState({
+    series: [
+      {
+        name: "series1",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+      {
+        name: "series2",
+        data: [11, 32, 45, 32, 34, 52, 41],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "area",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z",
+        ],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
+        },
+      },
+    },
+  });
+
+  const [piechart, setPiechart] = useState({
+    series: [44, 55, 41, 17, 15],
+    options: {
+      chart: {
+        type: "donut",
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200,
+            },
+            legend: {
+              position: "bottom",
+            },
+          },
+        },
+      ],
+    },
+  });
 
   return (
     <>
@@ -242,17 +324,60 @@ const Dashboard = () => {
                         </div>
                       </div>
                     </div>
-                    <div className="col-lg-9"></div>
+                    <div className="col-lg-9">
+                      <div className="row">
+                        <div className="col-lg-8"></div>
+                        <div className="col-lg-4 ">
+                          <div className="row">
+                            <div className="col-lg-5 mt-2">
+                              <b>Sort by:</b>
+                            </div>
+                            <div className="col-lg-7">
+                              <select
+                                className="form-select border-0"
+                                aria-label="Default select example"
+                              >
+                                <option defaultValue="0">Yearly</option>
+                                <option value="1">Weekaly</option>
+                                <option value="2">Monthly</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <Chart
+                        options={state.options}
+                        series={state.series}
+                        type="bar"
+                        width="500"
+                      />
+                    </div>
                   </div>
                 </div>
                 {/* overview section end */}
                 {/* user, order,product section start */}
                 <div className="row">
                   <div className="col-lg-4">
-                    <div className="shadow-lg p-1  bg-white rounded mt-5"></div>
+                    <div
+                      className="shadow-lg p-1  bg-white rounded mt-5"
+                      id="chart"
+                    >
+                      <ReactApexChart
+                        options={spchart.options}
+                        series={spchart.series}
+                        type="area"
+                        height={350}
+                      />
+                    </div>
                   </div>
                   <div className="col-lg-4">
-                    <div className="shadow-lg p-1  bg-white rounded mt-5"></div>
+                    <div className="shadow-lg p-1  bg-white rounded mt-5">
+                      <ReactApexChart
+                        options={piechart.options}
+                        series={piechart.series}
+                        type="donut"
+                      />
+                    </div>
                   </div>
                   <div className="col-lg-4">
                     <div className="shadow-lg p-1  bg-white rounded mt-5">
